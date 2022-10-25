86a3baf39d0138c96caa751009a7931012dbe7ab
Matheus <matheus_501@hotmail.com>
2022-10-24 22:23:29 -0300

    criando token de autenticacao

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 const NotesController = require("../controllers/NotesController");
 const notesRoutes = Router();
 const notesController = new NotesController();
-
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+notesRoutes.use(ensureAuthenticated);
 notesRoutes.get("/", notesController.index);
-notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.post("/", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 const usersController = new UsersController();
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = usersRoutes;
